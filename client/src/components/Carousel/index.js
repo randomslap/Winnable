92f42402ec994567e45ea2fbf3df586716671b21
davepump <46700142+dcabrera97@users.noreply.github.com>
2019-07-27 12:53:10 -0700

    added home page layout/set up stat page

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -1,13 +1,48 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
+import Carousel from 'react-bootstrap/Carousel'
 import "./index.css"
 
 
 const Jumbotron = () => {
-    
+
     return (
-        <Button variant="primary">Primary</Button>
-        
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://via.placeholder.com/150"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://via.placeholder.com/150"
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://via.placeholder.com/150"
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     )
 }
 
---
+++ client/src/pages/StatsPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import { InputGroup, FormControl, Container, Row, Col } from "react-bootstrap"
+
+class StatsPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default StatsPage
