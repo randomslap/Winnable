0b0ec6f9fc5b7becb1743c6ac6775d686a0e35c9
davepump <46700142+dcabrera97@users.noreply.github.com>
2019-07-31 14:43:51 -0700

    home header

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -6,43 +6,55 @@ import "./index.css"
 const Jumbotron = () => {
 
     return (
-        <Carousel>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://via.placeholder.com/150"
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://via.placeholder.com/150"
-                    alt="Third slide"
-                />
+        <div>
+            <div class="overlay"></div>
+            
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://via.placeholder.com/1200"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://via.placeholder.com/1200"
+                        alt="Third slide"
+                    />
 
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://via.placeholder.com/150"
-                    alt="Third slide"
-                />
+                    <Carousel.Caption>
+                        
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://via.placeholder.com/1200"
+                        alt="Third slide"
+                    />
 
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+                    <Carousel.Caption>
+                        
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://via.placeholder.com/1200"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
     )
 }
 
