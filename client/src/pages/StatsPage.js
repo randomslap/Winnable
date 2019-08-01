f861425f1fd500caf4ccf693a47ba7527434c569
Madrigalbit <46862219+Madrigalbit@users.noreply.github.com>
2019-07-31 21:53:38 -0700

    Statspage work

--- client/src/pages/StatsPage.js
+++ client/src/pages/StatsPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
-import { InputGroup, FormControl, Container, Row, Col, Card, Form, Button } from "react-bootstrap"
+import { InputGroup, FormControl, Container, Row, Col, Card, Form, Button, Image } from "react-bootstrap"
 import API from "../utils/API";
+import "./StatsPage.css"
 
 class StatsPage extends Component {
 
@@ -10,135 +11,143 @@ class StatsPage extends Component {
         userRank: "",
         rankPicture: "",
         userIcon: "",
-        userLevel:"",
+        userLevel: "",
         userSR: 0,
     };
-    
-      loadStats = () =>{
-          console.log("Is load stats getting hit?")
-        API.getStats(this.state.bnetName + "-" + this.state.bnetNum)
-        .then(res => {
-            console.log(res.data.us.stats.competitive.overall_stats)
-            this.setState({
-                userRank: res.data.us.stats.competitive.overall_stats.tier,
-                userLevel: res.data.us.stats.competitive.overall_stats.level,
-                userSR: res.data.us.stats.competitive.overall_stats.comprank
-            })
-        }
-        )}
 
-        handleInputChange = event =>{
+    loadStats = () => {
+        console.log("Is load stats getting hit?")
+        API.getStats(this.state.bnetName + "-" + this.state.bnetNum)
+            .then(res => {
+                console.log(res.data.us.stats.competitive.overall_stats)
+                this.setState({
+                    userRank: res.data.us.stats.competitive.overall_stats.tier,
+                    userLevel: res.data.us.stats.competitive.overall_stats.level,
+                    userSR: res.data.us.stats.competitive.overall_stats.comprank
+                })
+            }
+            )
+    }
+
+    handleInputChange = event => {
         let value = event.target.value;
         const name = event.target.name;
 
         // Updating the input's state
-            this.setState({
+        this.setState({
             [name]: value
-            });
-            console.log(name)
-            console.log(value)
-        }
+        });
+        console.log(name)
+        console.log(value)
+    }
 
-        handleFormSubmit = event => {
-            event.preventDefault();
-            this.loadStats()
-        };
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.loadStats()
+    };
 
     render() {
         return (
             <div>
-                <Container>
-                    <Row>
-                        <Col>
-                            <Card>
 
+                <Container className="pt-3">
+                    <Row>
+                        <Col md={12}>
+                            <Card>
+                                <Row>
+                                    <Col md={{ span: 3 }}>
+                                        <Row>
+                                            <Col>
+                                                <p id="searchBarText">CHECK PLAYER RANK AND STATS</p>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                    <Col md={{ span: 5 }}>
+                                        <Form.Control size="lg" type="text" placeholder="search user here..." />
+                                    </Col>
+                                    <Col md={{ span: 2 }}>
+                                        <Form.Control size="lg" type="text" placeholder="battletag..." />
+                                    </Col>
+                                    <Col md={{ span: 2 }}>
+                                        <Button size="lg" type="submit">SEARCH</Button>
+                                    </Col>
+                                </Row>
                             </Card>
                         </Col>
                     </Row>
+                </Container>
 
+                <Container className="pt-5">
                     <Row>
-                        <Col>
+                        <Col md={9}>
+                            <Card id="userInfoCard">
+                                <Row>
+                                    <Col md={{ span: 3, offset: 1 }}>
+                                        <Image src="https://place-hold.it/200x200" rounded />
+                                    </Col>
+                                    <Col md={{ span: 8 }}>
+                                        <h1>User_Name619<span>#8080</span></h1>
+                                    </Col>
+                                </Row>
+                            </Card>
+                        </Col>
+                        <Col md={3}>
                             <Card>
-                                <Card.Body>
-                                    <Form>
-                                        <Form.Group controlId="formBasicEmail">
-                                            <Form.Label>Name</Form.Label>
-                                            <Form.Control 
-                                                value={this.state.bnetName}
-                                                onChange={this.handleInputChange}
-                                                name="bnetName"
-                                                type="text" 
-                                                placeholder="Enter Battletag" />
-                                            <Form.Text className="text-muted">
-                                                Please make sure your Battletag is properly Capitalized
-                                            </Form.Text>
-                                        </Form.Group>
-
-                                        <Form.Group controlId="formBasicPassword">
-                                            <Form.Label>Number</Form.Label>
-                                            <Form.Control 
-                                            value={this.state.title}
-                                            onChange={this.handleInputChange}
-                                            name="bnetNum"
-                                            type="text" placeholder="1234" />
-                                        </Form.Group>
-                                        <Button onClick={this.handleFormSubmit} variant="primary" type="submit">
-                                            Submit
-                                            </Button>
-                                    </Form></Card.Body>
+                                <Row>
+                                    <Col md={12}>
+                                        <Row>
+                                            <Col md={12}>
+                                                <p>COMPETITIVE STATS</p>
+                                                <hr></hr>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col md={12}>
+                                                <p id="test">LEVEL: </p>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col md={12}>
+                                                <p id="test">RANK: </p>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col md={12}>
+                                                <p id="test">GAMES WON: </p>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col md={12}>
+                                                <p id="test">ENDORSMENT:</p>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Row>
                             </Card>
                         </Col>
                     </Row>
+                </Container>
 
+                <Container className="pt-5">
                     <Row>
-                        <Col>
+                        <Col md={12}>
                             <Card>
-                                <Card.Body>
-                                    <Container></Container>
-                                    <ul>
-                                        <li>Name: <span>{this.state.bnetName + "#" + this.state.bnetNum}</span></li>
-                                        <li>Level: <span>{this.state.userLevel}</span></li>
-                                        <li>Rank: <span>{this.state.userRank}</span></li>
-                                        <li>Rank SR: <span>{this.state.userSR}</span></li>
-                                    </ul>
-                                </Card.Body>
-                            </Card>
-                        </Col>
+                                <Row>
+                                    <Col md={6}>
+                                        <Row>
+                                            <Col md={{ span: 6 }}>
+                                                <Image src="https://place-hold.it/80x80" />
+                                            </Col>
+                                            <Col md={{ span: 6 }}>
+                                            <p id="test">LUCIO</p>
+                                            </Col>
+                                        </Row>
+                                    </Col>
 
-                        <Col>
-                            <Card>
-                                <Card.Body>This is some text within a card body.</Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
+                                    <Col md={6}>
 
-                    <Row>
-                        <Col>
-                            <Card>
-                                <Card.Body>This is some text within a card body.</Card.Body>
-                            </Card>
-                        </Col>
-
-                        <Col>
-                            <Card>
-                                <Card.Body>This is some text within a card body.</Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col>
-                            <Card>
-
-                                <Card.Body>This is some text within a card body.</Card.Body>
-                            </Card>
-                        </Col>
-
-                        <Col>
-                            <Card>
-                                <Card.Body>
-
-                                </Card.Body>
+                                    </Col>
+                                </Row>
                             </Card>
                         </Col>
                     </Row>
