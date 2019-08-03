9ca2a46554242ac2f56ee6f8ee76261b58070689
Logan Bertrand <loganwbertrand@gmail.com>
2019-08-03 14:26:22 -0700

    API page requests

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -17,5 +17,17 @@ export default {
 	},
 	getOWStats: function(gamertag) {
 		return axios.get(`/api/ow/${gamertag}`);
-	}
+	},
+	getTeams: function() {
+		return axios.get("/api/teams");
+	},
+	getTeam: function(id) {
+		return axios.get("/api/teams/" + id);
+	},
+	deleteTeam: function(id) {
+		return axios.delete("/api/teams/" + id);
+	},
+	saveTeam: function(teamData) {
+		return axios.post("/api/teams", teamData);
+	},
 };
