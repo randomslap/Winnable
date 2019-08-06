876c9e771abf10f86473691d107b0bccb7f0a10a
Davis Cabrera <davis.cabrera@gmail.com>
2019-08-06 03:51:59 -0700

    styled some of the homepage

--- client/src/components/FeatureSection/index.js
+++ client/src/components/FeatureSection/index.js
@@ -5,7 +5,8 @@ import "./index.css"
 const Features = () => {
     return (
         <Container className="text-white pt-5">
-            <h1 className="text-center pb-5">Features</h1>
+            <h2 className="text-center" id="featureheader">Features</h2>
+            <h5 className="text-center pb-5">Winnable is your one platform site for everything Overwatch.</h5>
             <Row>
                 <Col md={3}><i class="fas fa-binoculars fa-6x pt-3"></i>
                     <p className="pt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
