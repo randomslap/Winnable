a7deb1be16598a122a532af60919582962fbcc82
Davis Cabrera <davis.cabrera@gmail.com>
2019-08-06 19:08:06 -0700

    did some styl'n

--- client/src/pages/TeamProfile/index.js
+++ client/src/pages/TeamProfile/index.js
@@ -25,11 +25,11 @@ const TeamProfile = () => {
                     </Card>
                     <Card className="cardTwo">
                         <Row>
-                            <Col md={2}>
-                                <h3 className="">Team Owner:</h3>
+                            <Col md={3}>
+                                <h3 className="text-left">Team Owner:</h3>
                             </Col>
-                            <Col className="text-right" md={10}>
-                                <Button className="btn btn-primary">Apply <i class="fas fa-paper-plane"></i></Button>
+                            <Col className="text-right" md={9}>
+                                <Button className="btn btn-orange">Apply <i class="fas fa-paper-plane"></i></Button>
                             </Col>
                         </Row>
                         <Row>
@@ -38,38 +38,44 @@ const TeamProfile = () => {
                             </Col>
                         </Row>
                         <Row>
-                            <Col md={2}>
-                                <h3 className="pt-5">Players:</h3>
+                            <Col md={3}>
+                                <h3 className="pt-5 text-left">Roster:</h3>
                             </Col>
                         </Row>
                         <Row>
                             <Col md={2}>
                                 <img src="https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/d/d4/New_Tank_Icon.png/120px-New_Tank_Icon.png?version=0fda46c98855553e418631ade1d114ee" width="80" height="80"/>
+                                <a href="#"><p className="pt-2">cmndrshepard</p></a>
                             </Col>
                             <Col md={2}>
                                 <img src="https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/d/d4/New_Tank_Icon.png/120px-New_Tank_Icon.png?version=0fda46c98855553e418631ade1d114ee" width="80" height="80"/>
+                                <a href="#"><p className="pt-2">cmndrshepard</p></a>
                             </Col>
                             <Col md={2}>
                                 <img src="https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/1/1c/New_Damage_Icon.png/120px-New_Damage_Icon.png?version=a2c2d0fd3d02948d7664d63494a450f4" width="80" height="80"/>
+                                <a href="#"><p className="pt-2">cmndrshepard</p></a>
                             </Col>
                             <Col md={2}>
                                 <img src="https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/1/1c/New_Damage_Icon.png/120px-New_Damage_Icon.png?version=a2c2d0fd3d02948d7664d63494a450f4" width="80" height="80"/>
+                                <a href="#"><p className="pt-2">cmndrshepard</p></a>
                             </Col>
                             <Col md={2}>
                                 <img src="https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/f/f7/New_Support_Icon.png/120px-New_Support_Icon.png?version=ed01b0059c14bc180981f295ea4a1c7d" width="80" height="80"/>
+                                <a href="#"><p className="pt-2">cmndrshepard</p></a>
                             </Col>
                             <Col md={2}>
                                 <img src="https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/f/f7/New_Support_Icon.png/120px-New_Support_Icon.png?version=ed01b0059c14bc180981f295ea4a1c7d" width="80" height="80"/>
+                                <a href="#"><p className="pt-2">cmndrshepard</p></a>
                             </Col>
                         </Row>
                         <Row>
-                            <Col md={2}>
-                                <h3 className="pt-5">SR:</h3>
+                            <Col md={3}>
+                                <h3 className="pt-5 text-left">SR Range:</h3>
                             </Col>
                         </Row>
                         <Row>
-                            <Col md={2}>
-                            <img src="https://gamepedia.cursecdn.com/overwatch_gamepedia/thumb/1/1c/New_Damage_Icon.png/120px-New_Damage_Icon.png?version=a2c2d0fd3d02948d7664d63494a450f4" width="80" height="80"/>
+                            <Col md={3}>
+                            <h4 className="text-left">3500 - 4000</h4>
                             </Col>
                         </Row>
                     </Card>
